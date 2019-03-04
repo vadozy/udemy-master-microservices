10f325fa4cb45d6fe702010ec617c19ce213d476
Vadim Storozhuk <vadozy@yahoo.com>
2019-03-03 20:00:14 -0500

    End of Lecture 25 (End of Step 14). Inside Section 3 - Restful Web Services with Spring Boot

---
+++ springboot/restful-web-services/src/main/java/com/noms2/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.noms2.rest.webservices.restfulwebservices.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.noms2.rest.webservices.restfulwebservices.user.UserNotFoundException;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request)
+			throws Exception {
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleUserNotFoundExceptions(UserNotFoundException ex, WebRequest request)
+			throws Exception {
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+
+}
---
+++ springboot/restful-web-services/src/main/java/com/noms2/rest/webservices/restfulwebservices/user/UserNotFoundException.java
@@ -0,0 +1,18 @@
+package com.noms2.rest.webservices.restfulwebservices.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UserNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
