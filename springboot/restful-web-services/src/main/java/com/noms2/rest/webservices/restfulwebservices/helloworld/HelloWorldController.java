68836f4d494f086e9623321fa9faea5704d2b99f
Vadim Storozhuk <vadim.storozhuk@man.com>
2019-03-04 18:06:28 -0500

    End of Lecture 34 (End of Step 22). Inside Section 3 (i18n and Swagger done)

--- springboot/restful-web-services/src/main/java/com/noms2/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ springboot/restful-web-services/src/main/java/com/noms2/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,17 @@
 package com.noms2.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
@@ -20,5 +26,10 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path="/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 }
