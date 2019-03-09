c2123c4ef9a0e94f98728a816df2c0c155383b26
Vadim Storozhuk <vadozy@yahoo.com>
2019-03-09 14:40:07 -0500

    End of Lecture 59 (End of Step 3). Inside Section 4 (limits-service beginnigs)

---
+++ limits-service/src/main/java/com/noms2/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,20 @@
+package com.noms2.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.noms2.microservices.limitsservice.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsFromConfigurations() {
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+	
+}
---
+++ limits-service/src/main/java/com/noms2/microservices/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,27 @@
+package com.noms2.microservices.limitsservice.bean;
+
+public class LimitConfiguration {
+	
+	private int maximum;
+	private int minimum;
+	
+	
+	public LimitConfiguration() {
+		super();
+	}
+	
+	public LimitConfiguration(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+	
+	public int getMaximum() {
+		return maximum;
+	}
+
+	public int getMinimum() {
+		return minimum;
+	}
+
+}
