793d7580c40bc6a489726edcaae1574b0c0a02ad
Vadim Storozhuk <vadozy@yahoo.com>
2019-03-11 22:40:26 -0400

    End of Lecture 91 (End of Step 34). Inside Section 4 (Zuul)

--- currency-conversion-service/src/main/java/com/noms2/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/noms2/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -5,11 +5,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange-service")
-@RibbonClient(name="currency-exchange-service")
+@FeignClient(name="netflix-zuul-api-gateway-server")
+@RibbonClient(name="currency-exchange-service") // Is it still needed?
 public interface CurrencyExchangeServiceProxy {
 	
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean retrieveExchangeValue(
 			@PathVariable String from,
 			@PathVariable String to);
