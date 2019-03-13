138eafadbba046cf45e00926a4f0b5adb5f2a3a2
Vadim Storozhuk <vadim.storozhuk@man.com>
2019-03-13 17:09:34 -0400

    End of Lecture 103 (End of Step 44). End of Section 3 (Hystrix). End of Course

--- limits-service/src/main/java/com/noms2/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/noms2/microservices/limitsservice/LimitsConfigurationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.noms2.microservices.limitsservice.bean.LimitConfiguration;
 
 @RestController
@@ -15,6 +16,17 @@ public class LimitsConfigurationController {
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Not Available (Testing Fault Tolerance)");
+	}
+	
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		// Basic Default Behavior
+		return new LimitConfiguration(999, 9);
 	}
 	
 }
